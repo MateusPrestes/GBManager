5211d7aa9f8a549d01573adaef4e012f0ebe5bb9
MateusPrestes <mateus.prestes@gmail.com>
2013-07-04 13:00:57 -0300

    Tela Inicial ajustada

--- src/com/lp3/gbmanager/TelaInicial.java
+++ src/com/lp3/gbmanager/TelaInicial.java
@@ -2,10 +2,14 @@ package com.lp3.gbmanager;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class TelaInicial extends Activity {
 
+	Intent menu = new Intent(this, Menu_Adm.class);
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +21,12 @@ public class TelaInicial extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.tela_inicial, menu);
 		return true;
+	}
+	
+	public void Click(View v){
+		
+		startActivity(menu);
+		
 	}
 
 }
