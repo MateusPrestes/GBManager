d5df0b9718cfdf1668ec664af7b154d6a7acb063
Emanuella Soares <esmfreitas@gmail.com>
2013-07-04 21:33:26 -0300

    Tela de Nova Ordem de Servico

--- gen/com/lp3/gbmanager/R.java
+++ gen/com/lp3/gbmanager/R.java
@@ -8,6 +8,9 @@
 package com.lp3.gbmanager;
 
 public final class R {
+    public static final class array {
+        public static final int spinner1_entries=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -28,47 +31,63 @@ public final class R {
         public static final int newtask=0x7f020004;
     }
     public static final class id {
-        public static final int ImageButton02=0x7f080006;
-        public static final int LinearLayout1=0x7f080005;
-        public static final int LinearLayout2=0x7f080000;
-        public static final int action_settings=0x7f080017;
-        public static final int botao2=0x7f08000a;
-        public static final int botao3=0x7f08000f;
-        public static final int botoes=0x7f080015;
-        public static final int button2=0x7f080016;
-        public static final int editText1=0x7f080013;
-        public static final int editText2=0x7f080014;
-        public static final int imageButton1=0x7f08000b;
-        public static final int imageButton2=0x7f080007;
-        public static final int imageButton3=0x7f08000c;
-        public static final int imageButton4=0x7f080010;
-        public static final int imageButton5=0x7f080011;
-        public static final int imageView1=0x7f080001;
-        public static final int tela_login=0x7f080012;
-        public static final int textView1=0x7f080002;
-        public static final int textView2=0x7f080003;
-        public static final int textView3=0x7f080004;
-        public static final int textView4=0x7f080008;
-        public static final int textView5=0x7f080009;
-        public static final int textView6=0x7f08000d;
-        public static final int textView7=0x7f08000e;
+        public static final int ImageButton02=0x7f090006;
+        public static final int LinearLayout1=0x7f090005;
+        public static final int LinearLayout2=0x7f090000;
+        public static final int RelativeLayout1=0x7f090012;
+        public static final int action_settings=0x7f09001d;
+        public static final int botao2=0x7f09000a;
+        public static final int botao3=0x7f09000f;
+        public static final int botoes=0x7f09001c;
+        public static final int button1=0x7f090019;
+        public static final int button2=0x7f09001a;
+        public static final int editText1=0x7f090013;
+        public static final int editText2=0x7f090014;
+        public static final int editText3=0x7f090015;
+        public static final int editText4=0x7f090016;
+        public static final int editText5=0x7f090018;
+        public static final int imageButton1=0x7f09000b;
+        public static final int imageButton2=0x7f090007;
+        public static final int imageButton3=0x7f09000c;
+        public static final int imageButton4=0x7f090010;
+        public static final int imageButton5=0x7f090011;
+        public static final int imageView1=0x7f090001;
+        public static final int spinner1=0x7f090017;
+        public static final int tela_login=0x7f09001b;
+        public static final int textView1=0x7f090002;
+        public static final int textView2=0x7f090003;
+        public static final int textView3=0x7f090004;
+        public static final int textView4=0x7f090008;
+        public static final int textView5=0x7f090009;
+        public static final int textView6=0x7f09000d;
+        public static final int textView7=0x7f09000e;
     }
     public static final class layout {
         public static final int menu=0x7f030000;
-        public static final int tela_inicial=0x7f030001;
+        public static final int nova_ordem_servico=0x7f030001;
+        public static final int tela_inicial=0x7f030002;
     }
     public static final class menu {
-        public static final int menu__adm=0x7f070000;
-        public static final int tela_inicial=0x7f070001;
+        public static final int menu__adm=0x7f080000;
+        public static final int nova_ordem_servico=0x7f080001;
+        public static final int tela_inicial=0x7f080002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int btn_limpar=0x7f050006;
         public static final int btn_login=0x7f050005;
+        public static final int button_enviar=0x7f05001b;
+        public static final int button_limpar=0x7f05001c;
         public static final int configuracao=0x7f05000f;
         public static final int gbmanager=0x7f050002;
         public static final int hello_world=0x7f050008;
+        public static final int label_cliente=0x7f050014;
+        public static final int label_contrato=0x7f050015;
+        public static final int label_descricao=0x7f050017;
+        public static final int label_endereco=0x7f050016;
+        public static final int label_prazo=0x7f05001a;
+        public static final int label_usuario=0x7f050018;
         public static final int login=0x7f050003;
         public static final int logo_descricao=0x7f05000a;
         public static final int menu=0x7f050009;
@@ -78,8 +97,11 @@ public final class R {
         public static final int novo_cadastro=0x7f05000b;
         public static final int sair=0x7f050010;
         public static final int senha=0x7f050004;
+        public static final int spinner1_prompt=0x7f050019;
         public static final int title_activity_menu=0x7f050007;
         public static final int title_activity_menu__adm=0x7f050011;
+        public static final int title_activity_nova_ordem_servico=0x7f050012;
+        public static final int title_novaOrdemDeServico=0x7f050013;
     }
     public static final class style {
         /** 
@@ -103,10 +125,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/lp3/gbmanager/NovaOrdemServico.java
+++ src/com/lp3/gbmanager/NovaOrdemServico.java
@@ -2,28 +2,21 @@ package com.lp3.gbmanager;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
-public class Menu_Adm extends Activity {
+public class NovaOrdemServico extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.menu);
+		setContentView(R.layout.nova_ordem_servico);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu__adm, menu);
+		getMenuInflater().inflate(R.menu.nova_ordem_servico, menu);
 		return true;
-	}
-	
-	public void ClickOS (View v){
-		Intent os = new Intent(this,NovaOrdemServico.class);
-		startActivity(os);
 	}
 
 }
