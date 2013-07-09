8f61e959fd265394ef82b3a137d588e37c862f7a
ewertonj <ewertonj.oliveira@gmail.com>
2013-07-09 02:13:16 -0300

    tela cadastro de usuario

---
+++ src/com/lp3/gbmanager/CadastroUsuario.java
@@ -0,0 +1,26 @@
+package com.lp3.gbmanager;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class CadastroUsuario extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cadastro_usuario);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.cadastro_usuario, menu);
+		return true;
+	}
+	
+	
+
+}
