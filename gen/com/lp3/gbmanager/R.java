a55b28042da0405579025daa47e7d96bc07e4e84
Emanuella Soares <esmfreitas@gmail.com>
2013-09-02 23:50:49 -0300

    Preferencia Tela Inicial

--- gen/com/lp3/gbmanager/R.java
+++ gen/com/lp3/gbmanager/R.java
@@ -45,7 +45,7 @@ public final class R {
         public static final int LinearLayout2=0x7f090000;
         public static final int RelativeLayout1=0x7f090030;
         public static final int RelativeLayout1_OS=0x7f090034;
-        public static final int action_settings=0x7f090046;
+        public static final int action_settings=0x7f090047;
         public static final int botao2=0x7f090025;
         public static final int botao3=0x7f090028;
         public static final int botoes=0x7f090045;
@@ -55,6 +55,7 @@ public final class R {
         public static final int button1_OS=0x7f090042;
         public static final int button2=0x7f090015;
         public static final int button2_OS=0x7f090043;
+        public static final int chkSalvar=0x7f090046;
         public static final int cliente=0x7f09001d;
         public static final int contrato=0x7f09001e;
         public static final int descricao=0x7f090020;
@@ -123,71 +124,81 @@ public final class R {
         public static final int tela_inicial=0x7f03000a;
     }
     public static final class menu {
-        public static final int lista_atividades=0x7f080000;
+        public static final int cadastro_usuario=0x7f080000;
+        public static final int lista_atividades=0x7f080001;
+        public static final int menu__adm=0x7f080002;
+        public static final int minhas_tarefas=0x7f080003;
+        public static final int nova_ordem_servico=0x7f080004;
+        public static final int nova_tarefa=0x7f080005;
+        public static final int ordem_de_servico=0x7f080006;
+        public static final int tarefas=0x7f080007;
+        public static final int tela_inicial=0x7f080008;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int atividades_text=0x7f050029;
-        public static final int btn_limpar=0x7f050006;
+        public static final int atividades_text=0x7f05002b;
+        public static final int btn_fechar=0x7f050007;
+        public static final int btn_limpar=0x7f050008;
         public static final int btn_login=0x7f050005;
-        public static final int button_enviar=0x7f05001c;
-        public static final int button_enviarTarefa=0x7f050033;
-        public static final int button_iniciarOS=0x7f05003a;
-        public static final int button_iniciarTarefa=0x7f050035;
-        public static final int button_limpar=0x7f05001d;
-        public static final int button_limparTarefa=0x7f050031;
-        public static final int button_voltar=0x7f050036;
-        public static final int button_voltarDeOS=0x7f05003b;
-        public static final int configuracao=0x7f05000f;
-        public static final int email=0x7f050025;
-        public static final int enviar=0x7f05002b;
-        public static final int funcao=0x7f050026;
+        public static final int button_enviar=0x7f05001e;
+        public static final int button_enviarTarefa=0x7f050035;
+        public static final int button_iniciarOS=0x7f05003c;
+        public static final int button_iniciarTarefa=0x7f050037;
+        public static final int button_limpar=0x7f05001f;
+        public static final int button_limparTarefa=0x7f050033;
+        public static final int button_voltar=0x7f050038;
+        public static final int button_voltarDeOS=0x7f05003d;
+        public static final int chk_salvar=0x7f050006;
+        public static final int configuracao=0x7f050011;
+        public static final int email=0x7f050027;
+        public static final int enviar=0x7f05002d;
+        public static final int funcao=0x7f050028;
         public static final int gbmanager=0x7f050002;
-        public static final int hello_world=0x7f050008;
-        public static final int label_cliente=0x7f050015;
-        public static final int label_contrato=0x7f050016;
-        public static final int label_descricao=0x7f050018;
-        public static final int label_descricao2=0x7f050020;
-        public static final int label_endereco=0x7f050017;
-        public static final int label_minhasTarefas=0x7f050034;
-        public static final int label_novaTarefa=0x7f05001f;
-        public static final int label_prazo=0x7f05001b;
-        public static final int label_prazoTarefa=0x7f050032;
-        public static final int label_usuario=0x7f050019;
-        public static final int label_usuario2=0x7f050021;
-        public static final int limpar=0x7f05002c;
+        public static final int hello_world=0x7f05000a;
+        public static final int label_cliente=0x7f050017;
+        public static final int label_contrato=0x7f050018;
+        public static final int label_descricao=0x7f05001a;
+        public static final int label_descricao2=0x7f050022;
+        public static final int label_endereco=0x7f050019;
+        public static final int label_minhasTarefas=0x7f050036;
+        public static final int label_novaTarefa=0x7f050021;
+        public static final int label_prazo=0x7f05001d;
+        public static final int label_prazoTarefa=0x7f050034;
+        public static final int label_usuario=0x7f05001b;
+        public static final int label_usuario2=0x7f050023;
+        public static final int limpar=0x7f05002e;
         public static final int login=0x7f050003;
-        public static final int logo_descricao=0x7f05000a;
-        public static final int matricula=0x7f050024;
-        public static final int menu=0x7f050009;
-        public static final int monitoramento=0x7f05000e;
-        public static final int nova_os=0x7f05000d;
-        public static final int nova_tarefa=0x7f05000c;
-        public static final int novo_cadastro=0x7f05000b;
-        public static final int sair=0x7f050010;
+        public static final int logo_descricao=0x7f05000c;
+        public static final int matricula=0x7f050026;
+        public static final int menu=0x7f05000b;
+        public static final int monitoramento=0x7f050010;
+        public static final int nova_os=0x7f05000f;
+        public static final int nova_tarefa=0x7f05000e;
+        public static final int novo_cadastro=0x7f05000d;
+        public static final int sair=0x7f050012;
         public static final int senha=0x7f050004;
-        public static final int senhaCadastro=0x7f050027;
-        public static final int spinner1_OS_prompt=0x7f050038;
-        public static final int spinner1_prompt=0x7f05001a;
-        public static final int spinnerInicial=0x7f050028;
-        public static final int spinnerTarefa_prompt=0x7f050022;
-        public static final int tarefas=0x7f05002e;
-        public static final int title_activity_atividades=0x7f05003c;
-        public static final int title_activity_cadastro_usuario=0x7f050014;
-        public static final int title_activity_lista_atividades=0x7f05003d;
-        public static final int title_activity_menu=0x7f050007;
-        public static final int title_activity_menu__adm=0x7f050011;
-        public static final int title_activity_minhas_tarefas=0x7f050030;
-        public static final int title_activity_nova_tarefa=0x7f05001e;
-        public static final int title_activity_novaordemservico=0x7f050012;
-        public static final int title_activity_ordem_de_servico=0x7f050037;
-        public static final int title_activity_tarefas=0x7f05002d;
-        public static final int title_novaOrdemDeServico=0x7f050013;
-        public static final int title_ordemDeServico=0x7f050039;
-        public static final int usuario=0x7f050023;
-        public static final int voltar=0x7f05002f;
-        public static final int voltar2=0x7f05002a;
+        public static final int senhaCadastro=0x7f050029;
+        public static final int spinner1_OS_prompt=0x7f05003a;
+        public static final int spinner1_prompt=0x7f05001c;
+        public static final int spinnerInicial=0x7f05002a;
+        public static final int spinnerTarefa_prompt=0x7f050024;
+        public static final int tarefas=0x7f050030;
+        public static final int title_activity_atividades=0x7f05003e;
+        public static final int title_activity_cadastro_usuario=0x7f050016;
+        public static final int title_activity_lista_atividades=0x7f05003f;
+        public static final int title_activity_menu=0x7f050009;
+        public static final int title_activity_menu__adm=0x7f050013;
+        public static final int title_activity_minhas_tarefas=0x7f050032;
+        public static final int title_activity_nova_tarefa=0x7f050020;
+        public static final int title_activity_novaordemservico=0x7f050014;
+        public static final int title_activity_ordem_de_servico=0x7f050039;
+        public static final int title_activity_tarefas=0x7f05002f;
+        public static final int title_novaOrdemDeServico=0x7f050015;
+        public static final int title_ordemDeServico=0x7f05003b;
+        public static final int usuario=0x7f050025;
+        public static final int voltar=0x7f050031;
+        public static final int voltar2=0x7f05002c;
     }
     public static final class style {
         /** 
@@ -199,6 +210,17 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
